ddd2893feeb670324c26118f600c6c7475be9142
Rakhi <rakhid@cybage.com>
2024-12-18 12:02:58 +0530

    test solved

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,17 @@
+//import { render, screen } from '@testing-library/react';
+//import App from './App';
+
+//test('renders learn react link', () => {
+  //render(<App />);
+  //const linkElement = screen.getByText(/learn react/i);
+  //expect(linkElement).toBeInTheDocument();
+//});
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders calculator heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/calculator/i); // Match "Calculator" text
+  expect(headingElement).toBeInTheDocument();
 });
+
